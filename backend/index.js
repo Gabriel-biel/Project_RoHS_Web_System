5fdef79f4137b6526ab9a514cc4aaa70655e4f4e
mpvinnie <vinniemalafaia@hotmail.com>
2020-08-17 16:28:30 -0400

    Config Nodemon

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,26 @@ const express = require('express')
 
 const app = express()
 
-app.get('/', (request, response) => {
+app.use(express.json())
+
+/**
+ * Métodos HTTP:
+ * 
+ * GET: Buscar/listar uma informação do back-end
+ * POST: Criar uma informação no back-end
+ * PUT: Alterar uma informação no back-end
+ * DELETE: Deletar uma informação no back-end
+ */
+
+ /**
+  * Tipos de parametros:
+  * 
+  * Query Params: Parametros nomeados enviados na rota: (filtros, paginacao) -> const params = request.query
+  * Ruote Params: Parametros utilizados para identificar recursos (/users/:id) -> const params = request.params
+  * Request Body: Corpo da requisicao utilizado para criar ou alterar recursos -> request.body
+  */
+
+app.get('/users', (request, response) => {
   return response.json({ text: 'Hello World'})
 })
 

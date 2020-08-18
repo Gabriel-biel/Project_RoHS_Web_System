62b1d3586075a2e3d16a27156057db99d7888f0e
mpvinnie <vinniemalafaia@hotmail.com>
2020-08-17 21:47:49 -0400

    Clients Routes

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,35 @@
 const express = require('express')
+const crypto = require('crypto')
+
+const connection = require('./database/connection')
 
 const routes = express.Router()
 
-routes.get('/users', (request, response) => {
-  return response.json({ text: 'Hello World'})
+routes.get('/clients', async (request, response) => {
+  const clients = await connection('clients').select('*')
+
+  return response.json(clients)
+})
+
+routes.post('/clients', async (request, response) => {
+  const {
+    password,
+    name,
+    phone,
+    email
+  } = request.body
+
+  const id = crypto.randomBytes(4).toString('HEX')
+
+  await connection('clients').insert({
+    id,
+    password,
+    name,
+    phone,
+    email
+  })
+
+  return response.json({ id })
 })
 
 module.exports = routes

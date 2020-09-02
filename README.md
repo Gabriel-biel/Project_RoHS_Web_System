4ae1e7b75eef709f181b5687f364c5b93b79e76d
Gabriel-biel <gabriel97gla98@gmail.com>
2020-09-01 21:21:13 -0300

    Refatorando Projeto
    Refatoração.

--- README.md
+++
@@ -1,3 +0,0 @@
-# Rohs
-Repositório de desenvolvimento para a aplicação RoHS
-![Rohs](https://user-images.githubusercontent.com/61027045/91388074-fb349400-e80c-11ea-973d-df110cfe4f2e.png)
--- backend/src/database/connection.js
+++
@@ -1,8 +0,0 @@
-const { request } = require("express");
-
-const knex = require('knex')
-const configuration = require('../../knexfile')
-
-const connection = knex(configuration.development)
-
-module.exports = connection

c81c9617aa6af19620a46fd42e592f50e1ea949e
mpvinnie <vinniemalafaia@hotmail.com>
2020-09-25 07:16:08 -0400

    Reestructure SideBar

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,43 +1,17 @@
 import React from 'react';
 
 import Header from '../../components/Header'
+import SideBar from '../../components/SideBar'
 
-import companyIcon from '../../assets/icons/company.png'
-import documentsIcon from '../../assets/icons/documents.png'
-import chargerIcon from '../../assets/icons/charger.png'
-
-import { Container, Main, SideBar, ButtonSideBarContainer, ButtonSideBarIcon, ButtonSideBarText, Content } from './styles';
+import { Container, Main, Content } from './styles';
 
 const Dashboard = () => {
   return (
     <Container>
       <Header />
       <Main>
-        <SideBar>
-          <ButtonSideBarContainer>
-            <ButtonSideBarIcon>
-              <img src={companyIcon} alt="company icon" />
-            </ButtonSideBarIcon>
-            <ButtonSideBarText>Informações da compania</ButtonSideBarText>
-          </ButtonSideBarContainer>
-
-          <ButtonSideBarContainer style={{ background: '#92d050'}}>
-            <ButtonSideBarIcon>
-              <img src={documentsIcon} alt="company icon" />
-            </ButtonSideBarIcon>
-            <ButtonSideBarText>Gerenciamento de documentos</ButtonSideBarText>
-          </ButtonSideBarContainer>
-
-          <ButtonSideBarContainer style={{ background: '#92d050'}}>
-            <ButtonSideBarIcon>
-              <img src={chargerIcon} alt="company icon" />
-            </ButtonSideBarIcon>
-            <ButtonSideBarText>Gerenciamento de Partes</ButtonSideBarText>
-          </ButtonSideBarContainer>
-        </SideBar>
-
+        <SideBar />
         <Content>
-
         </Content>
       </Main>
     </Container>
--- frontend/src/pages/Dashboard/styles.js
+++ frontend/src/pages/Dashboard/styles.js
@@ -12,41 +12,4 @@ export const Main = styled.div`
   height: 92vh;
 `
 
-export const SideBar = styled.div`
-  background: #92d050;
-  width: 330px;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding-top: 20px;
-`
-
-export const ButtonSideBarContainer = styled.button`
-  width: 100%;
-  display: flex;
-  align-items: flex-start;
-  padding: 13px;
-  background: #a1e657;
-  border: 0;
-  transition: opacity .3s;
-  margin-bottom: 2px;
-
-  :hover {
-    opacity: 80%;
-  }
-`
-
-export const ButtonSideBarIcon = styled.div`
-  margin-right: 15px;
-  margin-top: 4px;
-`
-
-export const ButtonSideBarText = styled.div`
-  font-size: 18px;
-  font-weight: 700;
-  color: #fff;
-`
-
-
 export const Content = styled.div``

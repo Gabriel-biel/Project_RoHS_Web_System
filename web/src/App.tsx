7d31acb11025e777203470d00eb419fe0b3ab177
mpvinnie <vinniemalafaia@hotmail.com>
2020-10-20 04:12:32 -0400

    Config routes

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
 
-import Dashboard from './pages/Dashboard'
 import GlobalStyle from './styles/global'
+
+import Routes from './routes'
 
 const App: React.FC = () => {
   return (
-    <>
-      <Dashboard />
+    <Router>
+      <Routes />
       <GlobalStyle />
-    </>
+    </Router>
   )
 }
 

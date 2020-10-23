9818c750810c3c4f27dfb3904ce73c791dcb3c64
Gabriel-biel <gabriel97gla98@gmail.com>
2020-10-23 06:35:06 -0300

    Contacs

--- web/src/pages/Contacts/index.tsx
+++ web/src/pages/Contacts/index.tsx
@@ -1,12 +1,24 @@
 import React from 'react'
 
-import { Container } from './styles'
+import Sidebar from '../../components/Sidebar'
+import { Grid, Main, ButtonView } from './styles'
+import { FiPlusCircle } from 'react-icons/fi'
 
 const Contacts: React.FC = () => {
   return (
-    <Container>
-      <h1>Contacts</h1>
-    </Container>
+    <Grid>
+      <Sidebar page="ic" />
+
+      <Main>
+        <h1>Caro fornecedor, cadastre aqui suas informações princiapis para que sejam notificados sobre informações importantes.</h1>
+        <ButtonView>
+          <button>
+            <FiPlusCircle size={16} color="#fff"/>
+            Pressione
+          </button>
+        </ButtonView>
+      </Main>
+    </Grid>
   )
 }
 
--- web/src/pages/Contacts/styles.ts
+++ web/src/pages/Contacts/styles.ts
@@ -1,3 +1,30 @@
 import styled from 'styled-components'
 
-export const Container = styled.div``
+export const Grid = styled.div`
+  display: grid;
+
+  grid-template-columns: 300px auto;
+
+  grid-template-areas: 'SB MN';
+
+  height: 100vh;
+`
+
+export const Main = styled.div`
+
+`
+export const ButtonView = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+
+  padding: 0 32px;
+
+  button {
+    width: 174px;
+    height: 40px;
+  }
+`
+
+

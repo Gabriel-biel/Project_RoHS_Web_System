9c884f06342a1e421e409c9966784671c5355e56
mpvinnie <vinniemalafaia@hotmail.com>
2020-10-23 09:13:57 -0400

    create provider

--- server/src/controller/ProvidersControler.ts
+++ server/src/controller/ProvidersControler.ts
@@ -12,4 +12,43 @@ export default class ProvidersController {
 
     return response.status(200).json(providers)
   }
+
+  async create(request: Request, response: Response): Promise<Response> {
+    const { name, identifier, password, cnpj, location, segment } = request.body
+
+    const providersRepository = getRepository(Provider)
+
+    const verifyProviderById = await providersRepository.findOne({
+      where: { identifier }
+    })
+
+    if (verifyProviderById) {
+      return response.status(401).json({ error: 'Provider already exists' })
+    }
+
+    const verifyProviderByCnpj = await providersRepository.findOne({
+      where: { cnpj }
+    })
+
+    if (verifyProviderByCnpj) {
+      return response.status(401).json({ error: 'User already exists' })
+    }
+
+    try {
+      const provider = providersRepository.create({
+        name,
+        identifier,
+        password,
+        cnpj,
+        location,
+        segment
+      })
+
+      await providersRepository.save(provider)
+
+      return response.status(200).json(provider)
+    } catch (err) {
+      return response.status(401).json({ error: err.message })
+    }
+  }
 }
--- server/src/routes/provider.routes.ts
+++ server/src/routes/provider.routes.ts
@@ -7,5 +7,6 @@ const providersRouter = Router()
 const providersController = new ProvidersController()
 
 providersRouter.get('/', providersController.index)
+providersRouter.post('/', providersController.create)
 
 export default providersRouter
